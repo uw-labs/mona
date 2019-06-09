f174dafd141b16b5fb5f7c1abf0a0b37b9f0a0b2
David Bond <davidsbond93@gmail.com>
2019-06-09 17:12:39 +0100

    Added test for files.UpdateModuleFile

--- internal/files/module_test.go
+++ internal/files/module_test.go
@@ -91,5 +91,46 @@ func TestNewModuleFile(t *testing.T) {
 }
 
 func TestUpdateModuleFile(t *testing.T) {
+	tt := []struct {
+		Name          string
+		ModuleName    string
+		NewModuleData *files.ModuleFile
+	}{
+		{
+			Name:       "It should update a module file",
+			ModuleName: "test",
+			NewModuleData: &files.ModuleFile{
+				Name:     "test1",
+				Location: ".",
+				Exclude:  []string{"test"},
+			},
+		},
+	}
 
+	for _, tc := range tt {
+		t.Run(tc.Name, func(t *testing.T) {
+			if err := files.NewModuleFile(tc.ModuleName, "."); err != nil {
+				assert.Fail(t, err.Error())
+				return
+			}
+
+			if err := files.UpdateModuleFile(".", tc.NewModuleData); err != nil {
+				assert.Fail(t, err.Error())
+				return
+			}
+
+			mod, err := files.LoadModuleFile(".")
+
+			if err != nil {
+				assert.Fail(t, err.Error())
+				return
+			}
+
+			assert.EqualValues(t, tc.NewModuleData, mod)
+
+			if err := os.Remove("module.yml"); err != nil {
+				assert.Fail(t, err.Error())
+			}
+		})
+	}
 }
