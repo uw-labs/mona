8d54df7628ed5b33e44d1024cae39de4af0996b6
David Bond <davidsbond93@gmail.com>
2019-06-09 17:08:54 +0100

    Added test for files.LoadModuleFile

--- internal/files/module_test.go
+++ internal/files/module_test.go
@@ -9,7 +9,42 @@ import (
 )
 
 func TestLoadModuleFile(t *testing.T) {
+	tt := []struct {
+		Name       string
+		ModuleName string
+		Expected   *files.ModuleFile
+	}{
+		{
+			Name:       "It should load a module file",
+			ModuleName: "test",
+			Expected: &files.ModuleFile{
+				Name:     "test",
+				Location: ".",
+			},
+		},
+	}
 
+	for _, tc := range tt {
+		t.Run(tc.Name, func(t *testing.T) {
+			if err := files.NewModuleFile(tc.ModuleName, "."); err != nil {
+				assert.Fail(t, err.Error())
+				return
+			}
+
+			mod, err := files.LoadModuleFile(".")
+
+			if err != nil {
+				assert.Fail(t, err.Error())
+				return
+			}
+
+			assert.EqualValues(t, tc.Expected, mod)
+
+			if err := os.Remove("module.yml"); err != nil {
+				assert.Fail(t, err.Error())
+			}
+		})
+	}
 }
 
 func TestFindModules(t *testing.T) {
